8fa878a3a9a22f050d62520dac8f7d9f7aec2bcf
zhangsanfeng <zhang_chuanzhi@outlook.com>
2025-06-25 13:15:20 +0800

    build

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 const root = process.cwd();
-const pathResolve = (pathname) => resolve(root, '.', pathname);
+const pathResolve = (pathname) => resolve(root, ".", pathname);
 export default defineConfig({
+    base: "/shiptalk/",
     plugins: [vue()],
     server: {
-        allowedHosts: ['fwk3216.yunmv.cn', 'localhost', '127.0.0.1']
+        allowedHosts: ["fwk3216.yunmv.cn", "localhost", "127.0.0.1"],
     },
     resolve: {
         alias: {
-            '/@/': pathResolve('src') + '/',
-            '/#/': pathResolve('types') + '/',
+            "/@/": pathResolve("src") + "/",
+            "/#/": pathResolve("types") + "/",
         },
     },
+    build: {
+        outDir: "docs"
+    }
 });
